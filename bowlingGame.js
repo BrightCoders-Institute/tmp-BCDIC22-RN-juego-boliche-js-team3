8d99dbd769ac325a369e2010b0ce0e58f438b721
Gaddiel Patiño <alegaddiel@gmail.com>
2022-12-16 17:03:01 -0600

    Llenado de array
    Co-authored-by: github <garzo94>
    Co-authored-by: github <arthurchina92>

--- bowlingGame.js
+++ bowlingGame.js
@@ -7,27 +7,23 @@ function secondballRolled(firstBallRolled) {
   const secondKnockDown = Math.floor(Math.random() * (11 - firstBallRolled));
   return secondKnockDown;
 }
-const score = ballRolled();
-
-if (score === 10) {
-  console.log("Strike");
-} else if (score < 10) {
-  const secondScore = secondballRolled(score);
-  console.log(score, "score");
-  console.log(secondScore, "secondScore");
-  if (score + secondScore === 10) {
-    console.log("spare");
-  }
-}
 
 let totalScore;
 let gameTurns = [];
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i <=9; i++) {
+  let score = ballRolled();
+  
   if (score === 10) {
-    totalScore = 10;
+   //llenado de vector
+    gameTurns[i]= score;
+    console.log("turno " + i,gameTurns[i]= score);
+     
   } else if (score < 10) {
-    const secondScore = secondballRolled(score);
+    let secondScore = secondballRolled(score);
+   //llenado de vector
+    gameTurns[i]= score + secondScore;
+    console.log("turno " + i, gameTurns[i]= score + secondScore);
     console.log(score, "score");
     console.log(secondScore, "secondScore");
     if (score + secondScore === 10) {
