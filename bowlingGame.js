b571a080902b648ccb0081d4d69d0ebcfeb44875
garzo94 <alexgarzo25@gmail.com>
2022-12-19 15:53:10 -0600

    juego completado
     Co-authored-by: github <GaddPat609>
    Co-authored-by: github <arthurchina92>

--- bowlingGame.js
+++ bowlingGame.js
@@ -11,23 +11,84 @@ function secondballRolled(firstBallRolled) {
 let totalScore;
 let gameTurns = [];
 
-for (let i = 0; i <=9; i++) {
+for (let i = 0; i <= 9; i++) {
   let score = ballRolled();
-  
+  //   strike
   if (score === 10) {
-   //llenado de vector
-    gameTurns[i]= score;
-    console.log("turno " + i,gameTurns[i]= score);
-     
-  } else if (score < 10) {
+    gameTurns.push({ type: "strike", total: 10 });
+  }
+  //   spare
+  if (score < 10) {
     let secondScore = secondballRolled(score);
-   //llenado de vector
-    gameTurns[i]= score + secondScore;
-    console.log("turno " + i, gameTurns[i]= score + secondScore);
-    console.log(score, "score");
-    console.log(secondScore, "secondScore");
     if (score + secondScore === 10) {
-      console.log("spare");
+      gameTurns.push({
+        type: "spare",
+        first: score,
+        second: secondScore,
+        total: score + secondScore,
+      });
+    } else if (score + secondScore !== 10) {
+      gameTurns.push({
+        type: "normal",
+        first: score,
+        second: secondScore,
+        total: score + secondScore,
+      });
     }
   }
 }
+
+gameTurns.forEach((turn, index) => {
+  //  strike
+  if (turn.type === "strike" && index === 0) {
+    let next = gameTurns[index + 1].total;
+    let current = gameTurns[index].total;
+    gameTurns[index] = { ...turn, total: next + current };
+  } else if (turn.type === "strike" && index === 9) {
+    let previous = gameTurns[index - 1].total;
+    let current = gameTurns[index].total;
+    gameTurns[index] = { ...turn, total: previous + current };
+    const extra = ballRolled();
+    gameTurns["third"] = extra;
+    gameTurns[index] = { ...turn, total: previous + current + extra };
+  } else if (turn.type === "strike") {
+    let previous = gameTurns[index - 1].total;
+    let next = gameTurns[index + 1].total;
+    let current = gameTurns[index].total;
+    gameTurns[index] = { ...turn, total: previous + next + current };
+  }
+  // spare
+  if (turn.type === "spare" && index === 0) {
+    let next = gameTurns[index + 1].first;
+    let current = gameTurns[index].total;
+    gameTurns[index] = { ...turn, total: next + current };
+  } else if (turn.type === "spare" && index === 9) {
+    let previous = gameTurns[index - 1].total;
+    let current = gameTurns[index].total;
+    gameTurns[index] = { ...turn, total: previous + current };
+    const extra = ballRolled();
+    gameTurns["third"] = extra;
+    gameTurns[index] = { ...turn, total: previous + current + extra };
+  } else if (turn.type === "spare") {
+    let previous = gameTurns[index - 1].total;
+    let next = gameTurns[index + 1].first;
+    let current = gameTurns[index].total;
+    gameTurns[index] = { ...turn, total: previous + next + current };
+  }
+
+  // normal
+  if (turn.type === "normal" && index === 0) {
+    let current = gameTurns[index].total;
+    gameTurns[index] = { ...turn, total: current };
+  } else if (turn.type === "normal" && index === 9) {
+    let previous = gameTurns[index - 1].total;
+    let current = gameTurns[index].total;
+    gameTurns[index] = { ...turn, total: previous + current };
+  } else if (turn.type === "normal") {
+    let previous = gameTurns[index - 1].total;
+    let current = gameTurns[index].total;
+    gameTurns[index] = { ...turn, total: previous + current };
+  }
+});
+
+console.log(gameTurns);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+
+console.log(gameTurns, "game");
